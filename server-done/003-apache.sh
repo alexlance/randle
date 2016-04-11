e335dfe4ecb2811d8be7a9302ca6649451ca5bfe
Alex Lance <code@alexlance.com>
2016-04-11 21:02:03 +1000

    Ensure default apache conf is replaced

--- server-done/003-apache.sh
+++ server-done/003-apache.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
 # ensure our new default.conf got placed in
-grep 'DocumentRoot /var/www/html' /etc/apache2/sites-enabled/000-default.conf
+grep '# Hello World' /etc/apache2/sites-enabled/000-default.conf
 
--- server-todo/003-apache.sh
+++ server-todo/003-apache.sh
@@ -4,6 +4,7 @@
 rm -f /etc/apache2/sites-enabled/*
 cat << EOF > /etc/apache2/sites-enabled/000-default.conf
 <VirtualHost *:80>
+  # Hello World
   DocumentRoot /var/www/html
   ErrorLog ${APACHE_LOG_DIR}/error.log
   CustomLog ${APACHE_LOG_DIR}/access.log combined
