a88d0bcc3cc20447b7a3d953449418b2f2c77a66
Alex Lance <code@alexlance.com>
2016-05-06 15:24:13 +1000

    Add another check to ensure webserver is serving hello world app

--- server-done/040-install-helloworld.sh
+++ server-done/040-install-helloworld.sh
@@ -3,3 +3,6 @@ set -e
 
 # test that the helloworld app got deployed correctly
 grep -i hello /var/www/html/index.php
+
+# check that the webserver is serving out the app
+grep 'Hello, world!' <<<$(curl http://localhost)
